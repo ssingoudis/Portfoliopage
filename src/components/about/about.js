5882473ad5c6d78f0045c93bce834af9dca892d3
ssingoudis <ssingoudis@gmail.com>
2023-05-05 17:20:44 +0200

    updated about section

--- src/components/about/about.js
+++ src/components/about/about.js
@@ -4,6 +4,8 @@ import './about.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faSquareCheck } from '@fortawesome/free-solid-svg-icons'
 import Stavros_Singoudis_Logo from '../../img/Stavros_Singoudis_Logo.JPG'
+import testimg from '../../img/testimg.jpg'
+
 
 //import data
 import { skills } from '../../data';
@@ -26,27 +28,26 @@ const About = ({ removeHashFromUrl }) => {
   return (
     <div id='about' className='about'>
       <div className='rotating-logo-container'>
-      <img
-        src={Stavros_Singoudis_Logo}
-        alt=""
-        className='rotating-logo' />
-        </div>
-      <h1>Skills & Technologies</h1>
-      <hr className='heading' />
+        <h1>About me</h1>
+        <hr className='heading' />
+        <img
+          src={testimg}
+          alt="Profile-Picture"
+          className='rotating-logo' />
+      </div>
       <div className='divider'>
         <div className='left-side'>
-          <h2>About me</h2>
-
+          <h2>My Person</h2>
           <p className='heading-text'>I'm a <b>Software Enthusiast</b>, UI Designer & passionate Front-End focused Web Developer. My Design creates <b>great User Experiences</b> and leads to the overall success of the Product.<br /> <br />
-            I hold degrees in business informatics and <b>IT product management</b>, as well as professional experience as an IT consultant, requirements engineer, <b>product owner</b> and project manager.<br /><br/>
+            I hold degrees in business informatics and <b>IT product management</b>, as well as professional experience as an IT consultant, requirements engineer, <b>product owner</b> and project manager.<br /><br />
             At the turn of the year 2022, I decided to take a one-year trip through South America to see something of the world, unwind my soul and refresh my <b>programming skills</b> again.<br /> <br />
             Since then, I've made some websites but mostly worked on my own <button className='navigationBT' onClick={handleClickProjects}>projects</button>, since, as we all know, building projects is the most convenient way to learn.<br /> <br />
-            I am specialized in HTML, CSS & JavaScript and use as frameworks mainly <b>ReactJS</b>, sometimes TailwindCSS. As operating system I prefer <b>Windows</b>, while I feel most comfortable with <b>Git</b> in terms of version control systems.<br /><br />
+            I am specialized in <b>HTML</b>, <b>CSS</b> & <b>JavaScript</b> and use as frameworks mainly <b>ReactJS</b>, sometimes TailwindCSS. As operating system I prefer <b>Windows</b>, while I feel most comfortable with <b>Git</b> in terms of version control systems.<br /><br />
             Right now, <b>I am open to job opportunities</b> where I can contribute, learn and grow. If you have a good opportunity that matches my skills and experience, don't hesitate to <b>contact me</b>.</p>
           <button className='contact-button' onClick={handleClickContact}>Contact</button>
         </div>
         <div className='right-side'>
-          <h2>My Skills</h2>
+          <h2>Knowledge Stack</h2>
           <div className='container'>
             {skills.map((skill) => (
               <div key={skill.title} className='skill'>
